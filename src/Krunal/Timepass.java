b186129d65550e3bdb9324f2db8b3bbde26d1cd4
Krunal <krunalluhar5@gmail.com>
2024-07-25 11:09:14 +0530

    Day 1

---
+++ src/Krunal/Timepass.java
@@ -0,0 +1,10 @@
+package Krunal;
+
+public class Timepass {
+    public static void main(String[] args) {
+        int a =5;
+        int b= 5;
+        int sum = a+b;
+        System.out.println(sum);
+    }
+}

dd5212bc22e6b0ba5b20122b89a69802154e5ea4
Krunal <krunalluhar5@gmail.com>
2024-07-26 18:43:35 +0530

    Day 2

---
+++ src/Lecture_07/Switch_case.java
@@ -0,0 +1,37 @@
+package Lecture_07;
+
+import java.util.Scanner;
+
+public class Switch_case {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int EmpID = sc.nextInt();
+        String Department = sc.next();
+
+        switch (EmpID) {
+            case 1:
+                System.out.println("Krunal");
+                break;
+            case 2:
+                System.out.println("jassi");
+                break;
+            case 3:
+                System.out.println("Im emplyee 3");
+                switch(Department){
+                    case "IT":
+                        System.out.println("IT DEPARMENT");
+                        break;
+                    case "CS" :
+                        System.out.println("computer science");
+                        break;
+                    default:
+                        System.out.println("Enter valid deparment");
+
+                } break;
+            default:
+                System.out.println("Enter valid EmpID");
+                break;
+        }
+    }
+}
